0420e7408873f1f2479c1c65f520f671567ff8d6
jrdimande <timoteojrdimande@gmail.com>
2025-04-20 13:49:55 +0000

    Script to greet the user using their full name

---
+++ shell/greet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script to greet the user using their full name
+
+# Ask for first name
+read -p "Enter your first name: " first_name
+
+# Ask for the last name
+read -p "Enter your last name: " last_name
+
+# Displaying greeting
+echo "Hello, $first_name $last_name! welcome to the Linux system <3"
